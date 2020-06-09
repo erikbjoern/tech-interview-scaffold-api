95640200225c901eb1fb078e73194114896cf9cc
Erik Björn <erik.g.bjoern@gmail.com>
2020-06-09 18:34:15 +0200

    personalizing message

--- app/controllers/api/requests_controller.rb
+++ app/controllers/api/requests_controller.rb
@@ -1,5 +1,6 @@
 class Api::RequestsController < ApplicationController
   def create
-    render json: {text: 'https://hangouts.google.com/hangouts/_/cihvctunmrgqdfcbriuqh2b4e4e', response_type: "ephemeral", attachment: {text: 'Someone joined the Room.', response_type: 'in_channel'}}, status: 200
+    user = params[:user_id]
+    render json: {text: "Hey <@#{user>}>, you requested the room link: https://hangouts.google.com/hangouts/_/cihvctunmrgqdfcbriuqh2b4e4e", response_type: 'ephemeral'}, status: 200
   end
 end
